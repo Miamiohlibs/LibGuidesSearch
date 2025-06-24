3c5d2f17f3dc629ae2c096e537ab25ddd73dd3f5
Ken Irwin <ken@kenirwin.net>
2025-06-24 17:31:13 -0400

    getLibrarians outputs directly to cached file

--- getLibrarians.js
+++ getLibrarians.js
@@ -1,13 +1,19 @@
+/* 
+    This script fetches librarians from LibGuides using the LibGuides API
+    and saves the results to a file: ./cache/Librarians.json
+*/
+
 const LibGuidesAuth = require('./models/LibGuidesAuth');
 const config = require('config');
 const libGuidesConfig = config.get('libGuides');
 const libGuidesAuth = new LibGuidesAuth(libGuidesConfig);
 const axios = require('axios');
+const fs = require('fs');
 
 (async () => {
   try {
     const token = await libGuidesAuth.getAuthToken();
-    console.log('LibGuides Auth Token:', token.access_token);
+    // console.log('LibGuides Auth Token:', token.access_token);
 
     // Fetch librarians
     const response = await axios.get(
@@ -20,7 +26,10 @@ const axios = require('axios');
     );
 
     const librarians = response.data;
-    console.log(JSON.stringify(librarians, null, 2));
+    // Save librarians to a file
+    const librariansFile = './cache/Librarians.json';
+    fs.writeFileSync(librariansFile, JSON.stringify(librarians, null, 2));
+    console.log(`Librarians saved to ${librariansFile}`);
   } catch (error) {
     console.error('Error fetching librarians:', error.message);
   }
