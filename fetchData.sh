449f09a8edb99cae4be3cd89d651955d18a6fe53
Ken Irwin <ken@kenirwin.net>
2025-07-01 15:39:00 -0400

    formatting output for fetchData.sh

--- fetchData.sh
+++ fetchData.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Stage 1: Fetching data from the source URLs..."
 /usr/bin/env node ./stage1-fetchData.js
+echo
+echo "Stage 2: Collating data from the fetched files..."
 /usr/bin/env node ./stage2-collateData.js
+echo
+echo "Stage 3: Summarizing the collated data..."
 ./stage3-summarizeData.sh
+echo
+echo "Stage 4: Fetching guide contents..."
 ./stage4-LONG-fetchGuideContents.sh
