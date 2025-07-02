01f43dc16300a4f91852116527c493ee0ba55137
Ken Irwin <ken@kenirwin.net>
2025-07-02 13:04:48 -0400

    moved generateReport destination to public/docs

--- generateReport.js
+++ generateReport.js
@@ -3,6 +3,11 @@ const json2csv = require('json2csv').parse;
 let report = ReportController();
 const fs = require('fs');
 const path = require('path');
-const outputPath = path.join(__dirname, 'output', 'libGuidesSearchReport.csv');
+const outputPath = path.join(
+  __dirname,
+  'public',
+  'docs',
+  'libGuidesSearchReport.csv'
+);
 fs.writeFileSync(outputPath, json2csv(report.items), 'utf8');
 console.log(`Report generated and saved to ${outputPath}`);
