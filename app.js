015ba75abfb82f787c8a2bb2863e156de34af244
Ken Irwin <ken@kenirwin.net>
2025-07-02 13:05:07 -0400

    use public/docs for /report route if available

--- app.js
+++ app.js
@@ -46,20 +46,33 @@ app.get('/inspect/:id', async (req, res) => {
 });
 
 app.get('/report', (req, res) => {
-  const summary = require('./output/summary.json');
-  const ReportController = require('./controllers/ReportController');
-  let report = ReportController();
-  if (JSON.stringify(req.query).includes('json')) {
-    return res.send(report); // JSON response for API requests
-  }
-  // default to CSV output
-  const csv = json2csv(report.items);
-  res.header('Content-Type', 'text/csv');
-  res.setHeader(
-    'Content-Disposition',
-    'attachment; filename=libGuidesSearchReport.csv'
+  // if file exists in public/docs, deliver it
+  const filePath = path.join(
+    __dirname,
+    'public',
+    'docs',
+    'libGuidesSearchReport.csv'
   );
-  res.send(csv); // Send CSV file as response
+  if (require('fs').existsSync(filePath)) {
+    return res.download(filePath); // Download the report file
+  }
+  // If the file does not exist, generate the report
+  else {
+    const summary = require('./output/summary.json');
+    const ReportController = require('./controllers/ReportController');
+    let report = ReportController();
+    if (JSON.stringify(req.query).includes('json')) {
+      return res.send(report); // JSON response for API requests
+    }
+    // default to CSV output
+    const csv = json2csv(report.items);
+    res.header('Content-Type', 'text/csv');
+    res.setHeader(
+      'Content-Disposition',
+      'attachment; filename=libGuidesSearchReport.csv'
+    );
+    res.send(csv); // Send CSV file as response
+  }
 });
 
 // Uncomment the following lines to run the server
