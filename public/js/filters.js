2f84f300cb3f0245acf7ad7864560ab884540def
Ken Irwin <ken@kenirwin.net>
2025-07-02 15:39:33 -0400

    dynamically detect Type column in index view

--- public/js/filters.js
+++ public/js/filters.js
@@ -2,18 +2,22 @@ function buildStatusFilter() {
   var items = [],
     options = [];
 
+  // // Find the index of the "Type" column in the table header
+  const typeArrayIndex = $('#myTable thead tr th:contains("Type")').index();
+  const typeSelectorIndex = typeArrayIndex + 1; // Adjust for 1-based index in nth-child
+
   //Iterate all td's in third column
-  $('#myTable tbody tr td:nth-child(4)').each(function () {
+  $(`#myTable tbody tr td:nth-child(${typeSelectorIndex})`).each(function () {
     //add item to array
     items.push($(this).text());
   });
-  console.log('initial items: ' + items.length);
+  // console.log('initial items: ' + items.length);
 
   items = [...new Set(items.sort())];
   //restrict array to unique items
   // var items = $.unique( items );
   // var items = [new Set(...items)].flat();
-  console.log('unique items: ' + items.length);
+  // console.log('unique items: ' + items.length);
 
   //iterate unique array and build array of select options
   $.each(items, function (i, item) {
@@ -50,6 +54,10 @@ $.fn.dataTable.ext.search.push(function (
   rowData,
   counter
 ) {
+  // // Find the index of the "Type" column in the table header
+  const typeArrayIndex = $('#myTable thead tr th:contains("Type")').index();
+  const typeSelectorIndex = typeArrayIndex + 1; // Adjust for 1-based index in nth-child
+
   var selectedStatuses = [];
   $('#selectType input:checked').each(function () {
     selectedStatuses.push($(this).val());
